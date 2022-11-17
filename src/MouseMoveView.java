dbd5bfff68d14e7da9062f4448df6a9558c3cca7
jpfeiffer <jenny.pfeiffer@spheos.com>
2022-11-17 13:34:15 +0100

    styled mousemover that can be jard

--- src/MouseMoveView.java
+++ src/MouseMoveView.java
@@ -1,34 +1,69 @@
 import javax.swing.*;
+import javax.swing.border.LineBorder;
+import javax.swing.plaf.basic.BasicButtonUI;
 import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+import java.awt.event.WindowEvent;
 
 public class MouseMoveView extends JFrame {
     public MouseObserver serve;
+    private JLabel frame, border, grip, exit, minimize;
+    private Point initialClick;
+    private MouseMovement model;
 
     public MouseMoveView(MouseMovement model){
         super("Jennys awesome Mousemover");
+        this.model = model;
         setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
-        JLabel label = new JLabel("hier bewegt sich was");
+        try {
+            UIManager.setLookAndFeel(UIManager.getSystemLookAndFeelClassName());
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+        } catch (InstantiationException e) {
+            e.printStackTrace();
+        } catch (IllegalAccessException e) {
+            e.printStackTrace();
+        } catch (UnsupportedLookAndFeelException e) {
+            e.printStackTrace();
+        }
+        JLabel label = new JLabel("Press button to start or stop the mousemover. Have fun:)!", SwingConstants.CENTER);
+        label.setOpaque(true);
+        label.setBackground(new Color(255, 255, 255, 255));
+        label.setBounds(0, 0, 640, 480);
+        label.setSize(400,300);
         add(label);
+        JPanel panel = new JPanel(new GridLayout(2,1));
         JButton start  = new JButton("Start");
-        add(start);
+        start.setUI(new BasicButtonUI());
+        start.setBackground(new Color(255, 255, 255, 255));
+        start.setSize(600,100);
+        start.setBorder(new LineBorder(Color.GRAY));
+        panel.add(start);
         JButton stop = new JButton("Stop");
-        add(stop);
+        stop.setUI(new BasicButtonUI());
+        stop.setBackground(new Color(255, 255, 255, 255));
+        stop.setSize(600,50);
+        stop.setBorder(new LineBorder(Color.GRAY));
+        panel.add(stop);
+        panel.setBorder(new LineBorder(Color.GRAY));
         MouseController mousemove = new MouseController(model, label);
-        //MouseStopper stopper = new MouseStopper(model, label, this);
         MouseStopper stopper = new MouseStopper(model, label);
-        //this.serve = new MouseObserver(model, label);
 
         start.addActionListener(mousemove);
         stop.addActionListener(stopper);
-        setLayout(new GridLayout(3,1));
+        add(panel);
+        setLayout(new GridLayout(2,1));
         setLocation(300,50);
-        setSize(400,100);
+        setSize(600,400);
         setVisible(true);
     }
 
     public void stopObserver(){
         this.serve.stopThread();
     }
+
 
     public static void main(String []args){
 
